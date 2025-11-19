dancingmirrors/libplacebo

commit dc79ca113fbe5e5b068b924402d4bc006eb548a0

vulkan/swapchain: prefer bit depth over format in surface fmt selection
Fixes selecting 16-bits float formats for linear transfers.

Instead of:
VK_FORMAT_B8G8R8A8_UNORM + VK_COLOR_SPACE_DISPLAY_P3_LINEAR_EXT
we now select:
VK_FORMAT_R16G16B16A16_SFLOAT + VK_COLOR_SPACE_DISPLAY_P3_LINEAR_EXT

Note that SFLOAT formats are already filtered out for non-linear
transfers.

src/vulkan/swapchain.c
@@ -244,9 +244,9 @@ static bool pick_surf_format(pl_swapchain sw, const struct pl_color_space *hint)
             case PL_FMT_UNKNOWN: break;
             case PL_FMT_UINT: break;
             case PL_FMT_SINT: break;
-            case PL_FMT_UNORM: score += 500; break;
-            case PL_FMT_SNORM: score += 400; break;
-            case PL_FMT_FLOAT: score += 300; break;
+            case PL_FMT_UNORM: score += 3; break;
+            case PL_FMT_SNORM: score += 2; break;
+            case PL_FMT_FLOAT: score += 1; break;
             case PL_FMT_TYPE_COUNT: pl_unreachable();
             };
 

commit fd91c72ff8a8e3694015b23682dc2f873def5ccd

vulkan/swapchain: update scoring for surface formats
Don't filter formats and instead score them correctly to pick the best
one. Allowing fallback if something is not available for given transfer.

It's on user to decide what transfer format works for them and if
platform supports only 8-bit PQ than it's on them. Still libplacebo will
trie it's best to pick surface suitable for transfer.

Also no longer select 16-bit UNROM for SDR, this was regressed by
dc79ca113fbe5e5b068b924402d4bc006eb548a0. Because only SFLOAT formats
were filtered out.

Fixes: https://github.com/mpv-player/mpv/issues/13889

src/vulkan/swapchain.c
@@ -173,45 +173,6 @@ static bool pick_surf_format(pl_swapchain sw, const struct pl_color_space *hint)
         bool disable10 = !pl_color_transfer_is_hdr(space.transfer) &&
                          p->params.disable_10bit_sdr;
 
-        switch (p->formats.elem[i].format) {
-        // Only accept floating point formats for linear curves
-        case VK_FORMAT_R16G16B16_SFLOAT:
-        case VK_FORMAT_R16G16B16A16_SFLOAT:
-        case VK_FORMAT_R32G32B32_SFLOAT:
-        case VK_FORMAT_R32G32B32A32_SFLOAT:
-        case VK_FORMAT_R64G64B64_SFLOAT:
-        case VK_FORMAT_R64G64B64A64_SFLOAT:
-            if (space.transfer == PL_COLOR_TRC_LINEAR)
-                break; // accept
-            continue;
-
-        // Only accept 8 bit for non-HDR curves
-        case VK_FORMAT_R8G8B8_UNORM:
-        case VK_FORMAT_B8G8R8_UNORM:
-        case VK_FORMAT_R8G8B8A8_UNORM:
-        case VK_FORMAT_B8G8R8A8_UNORM:
-        case VK_FORMAT_A8B8G8R8_UNORM_PACK32:
-            if (!pl_color_transfer_is_hdr(space.transfer))
-                break; // accept
-            continue;
-
-        // Only accept 10 bit formats for non-linear curves
-        case VK_FORMAT_A2R10G10B10_UNORM_PACK32:
-        case VK_FORMAT_A2B10G10R10_UNORM_PACK32:
-            if (space.transfer != PL_COLOR_TRC_LINEAR && !disable10)
-                break; // accept
-            continue;
-
-        // Accept 16-bit formats for everything
-        case VK_FORMAT_R16G16B16_UNORM:
-        case VK_FORMAT_R16G16B16A16_UNORM:
-            if (!disable10)
-                break; // accept
-            continue;
-
-        default: continue;
-        }
-
         // Make sure we can wrap this format to a meaningful, valid pl_fmt
         for (int n = 0; n < gpu->num_formats; n++) {
             pl_fmt plfmt = gpu->formats[n];
@@ -227,8 +188,40 @@ static bool pick_surf_format(pl_swapchain sw, const struct pl_color_space *hint)
 
             // format valid, use it if it has a higher score
             int score = 0;
-            for (int c = 0; c < 3; c++)
-                score += plfmt->component_depth[c];
+            switch (plfmt->component_depth[0]) {
+                case 8:
+                    if (pl_color_transfer_is_hdr(space.transfer))
+                        score += 10;
+                    else if (space.transfer == PL_COLOR_TRC_LINEAR)
+                        continue; // avoid 8-bit linear formats
+                    else if (disable10)
+                        score += 30;
+                    else
+                        score += 20;
+                    break;
+                case 10:
+                    if (pl_color_transfer_is_hdr(space.transfer))
+                        score += 30;
+                    else if (space.transfer == PL_COLOR_TRC_LINEAR)
+                        continue; // avoid 10-bit linear formats
+                    else if (disable10)
+                        score += 20;
+                    else
+                        score += 30;
+                    break;
+                case 16:
+                    if (pl_color_transfer_is_hdr(space.transfer))
+                        score += 20;
+                    else if (space.transfer == PL_COLOR_TRC_LINEAR)
+                        score += 30;
+                    else if (disable10)
+                        score += 10;
+                    else
+                        score += 10;
+                    break;
+                default: // skip any other format
+                    continue;
+            }
             if (pl_color_primaries_is_wide_gamut(space.primaries) == wide_gamut)
                 score += 1000;
             if (space.transfer == PL_COLOR_TRC_LINEAR)